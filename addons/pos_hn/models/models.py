988f3c82207fcfa4eb3d347e1f1cddc1050be044
fermin <ferminarellano.hn@gmail.com>
2016-12-27 18:20:24 -0600

    Creacion de campos basicos para el CAI

--- addons/pos_hn/models/models.py
+++ addons/pos_hn/models/models.py
@@ -2,21 +2,11 @@
 
 from odoo import models, fields, api
 
-# class pos_hn(models.Model):
-#     _name = 'pos_hn.pos_hn'
+class cai(models.Model):
+	_name = "poshn.cai"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
-class res_company(models.Model):
-	_name = 'res.company'
-	_inherit = 'res.company'
-
-	codigo_cai = fields.Char(string="CAI", help="Ingrese su Código de Autorización de Impresión en este campo. Este saldrá automaticamente en su factura", size=64)
+	codigo_cai = fields.Char(string="CAI", help="Ingrese su Código de Autorización de Impresión en este campo. Este saldrá automaticamente en su factura", size=32)
 	fecha_limite_emision = fields.Date(string="Fecha limite emision", help="Ingrese la fecha limite de emisión para su código CAI")
+	rango_autorizado_desde = fields.Integer(string="Rango autorizado (desde)", help="Ingrese el rango de facturación autorizado por la DEI.")
+	rango_autorizado_hasta = fields.Integer(string="Rango autorizado (hasta)")
+	activo = fields.Boolean(string="Código Activo")
